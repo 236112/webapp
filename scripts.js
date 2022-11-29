670b9f4db6790bdfff7a6cc99adf209d9402fecb
Brandon <60393877+236112@users.noreply.github.com>
2022-11-29 13:04:40 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,25 +5,6 @@ function blueGold(){
     //if (    text == ('11/3/2021' || '11/5/2021'))
     if (
       [
-        "11/16/2022",
-        "11/18/2022",
-        "11/22/2022",
-        "11/29/2022",
-        "12/1/2022",
-        "12/5/2022",
-        "12/7/2022",
-        "12/9/2022",
-        "12/13/2022",
-        "12/15/2022",
-        "1/3/2023",
-        "1/5/2023",
-      ].includes(text)
-    ) {
-      document.getElementById("gold").style.display = "block";
-      document.getElementById("goldDate").innerHTML = text;
-    } else if (
-      [
-        
         "11/15/2022",
         "11/17/2022",
         "11/21/2022",
@@ -37,6 +18,26 @@ function blueGold(){
         "12/16/2022",
         "1/4/2022",
         "1/6/2023",
+      ].includes(text)
+    ) {
+      document.getElementById("gold").style.display = "block";
+      document.getElementById("goldDate").innerHTML = text;
+    } else if (
+      [
+        
+        
+        "11/16/2022",
+        "11/18/2022",
+        "11/22/2022",
+        "11/29/2022",
+        "12/1/2022",
+        "12/5/2022",
+        "12/7/2022",
+        "12/9/2022",
+        "12/13/2022",
+        "12/15/2022",
+        "1/3/2023",
+        "1/5/2023",
       ].includes(text)
     ) {
       document.getElementById("blue").style.display = "block";
